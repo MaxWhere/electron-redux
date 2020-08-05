0e1b8711559d408050b244f0a0291183c8a4ad10
pergy <persa.gyorgy@mistems.hu>
2020-08-05 08:00:01 +0200

    action.meta can have 'ignoreBlacklist' prop to skip blacklist check

--- packages/electron-redux/src/middleware/forwardToMain.js
+++ packages/electron-redux/src/middleware/forwardToMain.js
@@ -7,7 +7,7 @@ export const forwardToMainWithParams = (params = {}) => store => next => (action
   if (!validateAction(action)) return next(action);
   if (action.meta && action.meta.scope === 'local') return next(action);
 
-  if (blacklist.some(rule => rule.test(action.type))) {
+  if ((!action.meta || !action.meta.ignoreBlacklist) && blacklist.some(rule => rule.test(action.type))) {
     return next(action);
   }
 
